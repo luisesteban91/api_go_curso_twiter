7ee8ca66e898e3c056ce411165f93c6f912639d6
Luis Esteban <lcuacua@bienparabien.com>
2021-07-31 14:40:02 -0500

    agregar .env variables de entorno

--- bd/conectionBD.go
+++ bd/conectionBD.go
@@ -3,20 +3,30 @@ package bd
 import (
 	"context"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 /*MongoCon es el objecto de conexion a la BD*/
 var MongoCon = ConnectDB()
-var clientOptions = options.Client().ApplyURI("mongodb+srv://dbLecch:<password>@cluster0.w4xh2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 
 /*
 * ConnectBD es la funcion para conectar con la BD
 * return client
  */
 func ConnectDB() *mongo.Client {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	GO_PASSWORD_BD := os.Getenv("GO_PASSWORD_BD") //obetener variable de entorno
+
+	var clientOptions = options.Client().ApplyURI("mongodb+srv://dbLecch:" + GO_PASSWORD_BD + "@cluster0.w4xh2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+
 	Client, err := mongo.Connect(context.TODO(), clientOptions) //context.TODO() siginifica sin restricion de timeout, ect
 
 	if err != nil {
